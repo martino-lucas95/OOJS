1b15db296d2c774aaf3c8b803b99db98825f9910
martino-lucas95 <71530057+martino-lucas95@users.noreply.github.com>
2022-09-03 16:51:33 -0300

    constructor createad

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 const person = {
-    name: ['Lucas', 'Guillermo'],
+    name: {
+        first: 'Lucas',
+        last: 'Martino',
+    },
     age: 27,
     bio() {
-        console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+        console.log(`${this.name.first} ${this.name.last} is ${this.age} years old.`);
     },
     introduceSelf() {
-        console.log(`Hi! I'm ${this.name[0]}.`);
-    }
+        console.log(`Hi! I'm ${this.name.first}.`);
+    },
 };
+
+const myDataName = 'height';
+const myDataValue = '1.75m';
+person[myDataName] = myDataValue;
+
+
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+    this.introduceSelf = function () {
+        console.log(`Hi! I'm ${this.name}.`);
+      }
+}
+  
